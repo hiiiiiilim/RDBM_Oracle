4f932578af05e56ad73e204959efcafec194b078
hiiiiiilim <83237288+hiiiiiilim@users.noreply.github.com>
2023-10-11 16:33:44 +0900

    Update function_1.sql

--- function_1.sql
+++ function_1.sql
@@ -76,6 +76,24 @@ select ename, round(salary,2) as round_salary from employee;
 select ename, abs(commission) as ABS_COMMITSSION 
 FROM employee;
 
+--WINDOW :  sql에서 사용되는 분석 함수, 결과 집합 내에서 행의 순서와 관련된 계산을 수행할 수 있음
+            집계, 순위, 평균, 합계 등을 계산할때 유용
+            데이터를 파티션별로 분할하고 순서를 정의한 다음 각 파티션 내에 계산을 수행
+      <윈도우함수>(<표현식>)over 
+      partition by <파티션열>
+      order by <정렬 열>
+      rows between <시작 행> and <끝 행>
+  <윈도우함수>: 사용할 윈도우 함수 작성 ex) sum, av, rank등
+  <표현식>: 계산을 수행할 열 또는 식
+  partition by : 데이터를 파티션으로 나누는데 사용할 열을 지정
+  order by: 각 파티션 내에서 행 순서를 정의하는데 사용할 열을 지정
+  rows between : 계산을 수행할 행의 범위를 정의하는데 사용
+select 
+    order_id, order_date, total_price, 
+    sum(total_price) over (order by order_date) as od --이부분이 윈도우 함수,
+    --order_date를 기준으로 각 주문의 총 가격을 누적한 값을 반환한 것 결과 집합에는 order_id, order_date, total_price, od 열이 포함
+    -- 각 주문에 대한 누적 가격을 계산할 수 있음
+from orders;
 
  
  
